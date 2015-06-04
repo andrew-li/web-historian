53f689d8e30688121ee3c4eb16213547e38a29c0
andrew-li <ali91007@gmail.com>
2015-06-03 17:37:59 -0700

    finished isURLArchived

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -25,17 +25,91 @@ exports.initialize = function(pathsObj){
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-exports.readListOfUrls = function(){
+exports.readListOfUrls = function(callback){ //filename
+//gets the file as a string and splits it into lines
+  fs.readFile(exports.paths.list, function(err, text){
+    if (err) throw err;
+    callback(text.toString().split('\n'));
+  });
 };
 
-exports.isUrlInList = function(){
+exports.isUrlInList = function(target, callback){ //url, filename
+//checks if url is in the list
+  exports.readListOfUrls(function(data){
+    var bool = false;
+    for(var i = 0; i < data.length; ++i)
+    {
+      if(data[i].indexOf(target) >= 0)
+      {
+        bool = true;
+        break;
+      }
+    }
+    callback(bool);
+  });
 };
 
-exports.addUrlToList = function(){
+exports.addUrlToList = function(target){ //url, filename
+//adds url to the list
+////defaults archived to no
+  exports.isUrlInList(target, function(bool) {
+    if(bool === false)
+    {
+      fs.appendFile(exports.paths.list, target + ', 0\n', function (err) {
+        if (err) throw err;
+      });
+    }
+  });
 };
 
-exports.isURLArchived = function(){
+exports.isURLArchived = function(target, callback){ //url, filename
+//checks if url is archived
+  exports.readListOfUrls(function(data){
+    var bool = false;
+    for(var i = 0; i < data.length; ++i)
+    {
+      if(data[i].indexOf(target + ', 1') >= 0)
+      {
+        bool = true;
+        break;
+      }
+    }
+    callback(bool);
+  });
 };
 
-exports.downloadUrls = function(){
+/*
+      var callback = function(response) {
+        var str = '';
+
+        //another chunk of data has been recieved, so append it to `str`
+        response.on('data', function (chunk) {
+          str += chunk;
+        });
+
+        //the whole response has been recieved, so we just print it out here
+        response.on('end', function () {
+          httpHelpers.headers['Content-Type'] = 'text/html';
+          sendResponse(oldResponse, str, undefined, httpHelpers.headers);
+        });
+      }
+
+      options.host = pathname.slice(1);
+
+      http.request(options, callback).end();
+*/
+
+exports.downloadUrls = function(callback){ //filename
+//does the get request and saves the str as a file
+//updates no to yes
+  exports.readListOfUrls(function(data){
+    for(var i = 0; i < data.length; ++i)
+    {
+      if(data[i].charAt(data[i].length - 1) === '0')
+      {
+        console.log("here");
+      }
+    }
+    callback(bool);
+  });
 };
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,109 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+var httpHelpers = require('./http-helpers.js');
+var urlParser = require('url');
+var http = require('http');
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+var sendResponse = function(response, data, statusCode, headers){
+  statusCode = statusCode || 200;
+  response.writeHead(statusCode, headers);
+  response.end(data);
+};
+
+/*
+exports.collectData = function(request, callback){
+  var data = "";
+  request.on('data', function(chunk){
+    data += chunk;
+  });
+  request.on('end', function(){
+    callback(JSON.parse(data));
+});
+*/
+
+var options = {
+  host: '',
+  path: ''
+};
+
+var actions = {
+  'GET': function(request, response, pathname){
+    console.log(pathname);
+    var oldResponse = response;
+
+    if (pathname === '/')
+    {
+      var fullPath = path.join(__dirname, '../web/public/index.html');
+      httpHelpers.serveAssets(response, fullPath, function(err, html){
+        if (err) throw err;
+        httpHelpers.headers['Content-Type'] = 'text/html';
+        sendResponse(response, html, undefined, httpHelpers.headers);
+      });
+    }
+    else if (pathname === '/styles.css')
+    {
+      var fullPath = path.join(__dirname, '../web/public/styles.css');
+      httpHelpers.serveAssets(response, fullPath, function(err, css){
+        if (err) throw err;
+        httpHelpers.headers['Content-Type'] = 'text/css';
+        sendResponse(response, css, undefined, httpHelpers.headers);
+      });
+    }
+    else
+    {
+      archive.isUrlInList('www.hackreactor.com', function(bool) {
+         console.log(bool);
+      });
+
+      archive.isURLArchived('www.hackreactor.com', function(bool) {
+         console.log(bool);
+      });
+
+      archive.addUrlToList('www.hackreactors.com');
+    }
+    /*
+    else if (pathname.slice(0,4) === "/www")
+    {
+      var callback = function(response) {
+        var str = '';
+
+        //another chunk of data has been recieved, so append it to `str`
+        response.on('data', function (chunk) {
+          str += chunk;
+        });
+
+        //the whole response has been recieved, so we just print it out here
+        response.on('end', function () {
+          httpHelpers.headers['Content-Type'] = 'text/html';
+          sendResponse(oldResponse, str, undefined, httpHelpers.headers);
+        });
+      }
+
+      options.host = pathname.slice(1);
+
+      http.request(options, callback).end();
+    }*/
+  },
+  'POST': function(request, response, pathname){
+    console.log(pathname);
+    //sendResponse(response, archive.paths.list, 201);
+
+  },
+  'OPTIONS': function(request, response, pathname){
+    //sendResponse(response);
+  }
+};
+
+exports.handleRequest = function (request, response) {
+
+  var parts = urlParser.parse(request.url);
+
+  var action = actions[request.method];
+  if( action ){
+    action(request, response, parts.pathname);
+  } else {
+    sendResponse(response, "Not Found", 404);
+  }
 };
